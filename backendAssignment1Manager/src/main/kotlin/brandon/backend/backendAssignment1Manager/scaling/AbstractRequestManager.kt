36052d955e0d9921c06500f0e92a4341c886ad64
brandon <brandonfox@hotmail.co.uk>
2020-01-12 13:22:02 +0700

    Fixed concurrency issues.

--- backendAssignment1Manager/src/main/kotlin/brandon/backend/backendAssignment1Manager/scaling/AbstractRequestManager.kt
+++ backendAssignment1Manager/src/main/kotlin/brandon/backend/backendAssignment1Manager/scaling/AbstractRequestManager.kt
@@ -3,6 +3,7 @@ package brandon.backend.backendAssignment1Manager.scaling
 import brandon.backend.backendAssignment1Manager.webResult.WebResult
 import org.slf4j.LoggerFactory
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.collections.HashMap
 
 abstract class AbstractRequestManager {
@@ -10,47 +11,57 @@ abstract class AbstractRequestManager {
     //TODO Make request managers use same cache
 
     protected val timeoutTime = 60000 //1 min in milliseconds
-    private val cacheSizeLimit = 3 //TODO change 5 to bigger number after testing
+    private val cacheSizeLimit = 1000 //TODO change 5 to bigger number after testing
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    private val cacheMap: HashMap<String,WebResult> = HashMap()
+    //TODO Move cache stuff into another class
+
+    protected val cacheMap: HashMap<String,WebResult> = HashMap()
     private val cacheTimeMap = PriorityQueue<Pair<String,Long>>(UrlTimeComparator())
 
-    private val urlRequests = HashMap<String, ArrayList<(r: WebResult) -> Unit>>()
+    private val urlRequests = ConcurrentHashMap<String, LinkedList<(r: WebResult) -> Unit>>()
     protected val requestQ: LinkedList<Pair<String, (r: WebResult) -> Unit>> = LinkedList()
     private val urlsRequested = HashSet<String>()
 
     protected abstract fun processNextRequest()
+    protected abstract fun processCacheRequest(url:String,callback: (r: WebResult) -> Unit)
 
-    private fun initRequest(url:String, callback: (r: WebResult) -> Unit){
+    /**
+     * Initialises request for queue and others. Returns whether the request has already been requested
+     */
+    private fun initRequest(url:String, callback: (r: WebResult) -> Unit) : Boolean{
+        urlRequests.putIfAbsent(url,LinkedList())
+        logger.info("Adding another request to $url. Current size: ${urlRequests[url]!!.size}")
+        urlRequests[url]!!.add(callback)
         if(!urlsRequested.contains(url)){
             requestQ.add(Pair(url, callback))
+            urlsRequested.add(url)
+            return false
         }
-        urlRequests.putIfAbsent(url,ArrayList())
-        urlRequests[url]!!.add(callback)
-        urlsRequested.add(url)
+        return true
     }
 
     fun addRequest(url: String, callback: (r: WebResult) -> Unit, force: Boolean){
         if(force || System.currentTimeMillis() - (cacheMap[url]?.resultTime ?: 0) > ThreadRequestManager.timeoutTime){
             logger.info("Starting new read request at url: $url")
-            initRequest(url,callback)
-            processNextRequest()
+            if(!initRequest(url,callback))
+                processNextRequest()
         }
         else{
             logger.info("Sending cached result for url: $url")
-            callback(cacheMap[url]!!)
+            processCacheRequest(url,callback)
         }
     }
 
     fun finishRequest(url: String, result: WebResult){
-        for(rc in urlRequests[url]!!){
-            rc(result)
+        updateCache(url,result)
+
+        logger.info("Sending result to ${urlRequests[url]!!.size} clients")
+
+        while(urlRequests[url]!!.isNotEmpty()){
+            urlRequests[url]!!.poll()(result)
         }
         urlsRequested.remove(url)
-        urlRequests[url]!!.clear()
-
-        updateCache(url,result)
 
         processNextRequest()
     }
@@ -75,7 +86,5 @@ abstract class AbstractRequestManager {
             if(o1!!.first == o2!!.first) return 0
             return o1.second.compareTo(o2.second)
         }
-
     }
-
 }
--- backendAssignment1Manager/src/main/kotlin/brandon/backend/backendAssignment1Manager/scaling/ThreadRequestManager.kt
+++ backendAssignment1Manager/src/main/kotlin/brandon/backend/backendAssignment1Manager/scaling/ThreadRequestManager.kt
@@ -1,11 +1,12 @@
 package brandon.backend.backendAssignment1Manager.scaling
 
 import brandon.backend.backendAssignment1Manager.webResult.WebCounter
+import brandon.backend.backendAssignment1Manager.webResult.WebResult
 import java.util.concurrent.Executors
 
 object ThreadRequestManager : AbstractRequestManager() {
 
-    private const val maxThreads = 5
+    private const val maxThreads = 15
 
     //Idk to use cached or fixed threadpool
     private val threadPool = Executors.newFixedThreadPool(maxThreads)
@@ -15,5 +16,9 @@ object ThreadRequestManager : AbstractRequestManager() {
             val next = requestQ.poll()
             threadPool.execute(WebCounter(next.first) { finishRequest(next.first, it) })
         }
+    }
+
+    override fun processCacheRequest(url: String, callback: (r: WebResult) -> Unit) {
+        threadPool.execute {callback(cacheMap[url]!!)}
     }
 }
